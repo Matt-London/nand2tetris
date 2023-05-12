bf69a1a11f9279e6e005486707a8b2e01439d92f
Matt London <mrl2534@rit.edu>
2023-05-12 02:23:44 -0400

    Add enums to handle token and keyword types

---
+++ programs/jackcompiler/pkg/common/keyword.go
@@ -0,0 +1,40 @@
+package common
+
+// TokenType is an enum for the type of token
+type TokenType int
+
+const (
+	None TokenType = iota
+	Keyword
+	Symbol
+	IntegerConstant
+	StringConstant
+	Identifier
+)
+
+// KeywordType is an enum for type of keyword a token has (if it is a keyword)
+type KeywordType int
+
+const (
+	Class KeywordType = iota
+	Method
+	Function
+	Constructor
+	Int
+	Boolean
+	Char
+	Void
+	Var
+	Static
+	Field
+	Let
+	Do
+	If
+	Else
+	While
+	Return
+	True
+	False
+	Null
+	This
+)
