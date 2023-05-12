581bfd86f2d5fb9623171ae444e81bffa394f8b8
Matt London <mrl2534@rit.edu>
2023-05-12 02:24:01 -0400

    Write quick test to print all tokens

--- programs/jackcompiler/cmd/compiler/main.go
+++ programs/jackcompiler/cmd/compiler/main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"fmt"
+	. "jackcompiler/pkg/analyzer"
+	. "jackcompiler/pkg/common"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	tokenizer := NewTokenizer("tests/ArrayTest/Main.jack")
+
+	for tokenizer.HasMoreTokens() {
+		token := tokenizer.NextToken()
+		switch token.TokenType() {
+		case Keyword:
+			fmt.Println(token.KeywordType())
+		case Symbol:
+			fmt.Println(string(token.Symbol()))
+		case IntegerConstant:
+			fmt.Println(token.IntVal())
+		case StringConstant:
+			fmt.Println(token.StringVal())
+		case Identifier:
+			fmt.Println(token.Identifier())
+		}
+	}
 
 }
