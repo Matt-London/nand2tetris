ed9257f71ec8f89d4ad7b10e620c4247f5382c37
Matt London <mrl2534@rit.edu>
2023-05-13 01:08:22 -0400

    Setup main to compile a test

--- programs/jackcompiler/cmd/compiler/main.go
+++ programs/jackcompiler/cmd/compiler/main.go
@@ -1,28 +1,12 @@
 package main
 
 import (
-	"fmt"
 	. "jackcompiler/pkg/analyzer"
-	. "jackcompiler/pkg/common"
 )
 
 func main() {
-	tokenizer := NewTokenizer("tests/ArrayTest/Main.jack")
+	analyzer := NewAnalyzer("testsButcher/Square/Square.jack")
 
-	for tokenizer.HasMoreTokens() {
-		token := tokenizer.NextToken()
-		switch token.TokenType() {
-		case Keyword:
-			fmt.Println(token.KeywordType())
-		case Symbol:
-			fmt.Println(string(token.Symbol()))
-		case IntegerConstant:
-			fmt.Println(token.IntVal())
-		case StringConstant:
-			fmt.Println(token.StringVal())
-		case Identifier:
-			fmt.Println(token.Identifier())
-		}
-	}
+	analyzer.Analyze()
 
 }
