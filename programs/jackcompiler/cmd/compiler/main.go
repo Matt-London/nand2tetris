84c9c7c931e4e64165a91230599e7c328a5eb43d
Matt London <mrl2534@rit.edu>
2023-05-13 23:36:23 -0400

    Process command line arguments

--- programs/jackcompiler/cmd/compiler/main.go
+++ programs/jackcompiler/cmd/compiler/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	. "jackcompiler/pkg/analyzer"
+	"os"
 )
 
 func main() {
-	analyzer := NewAnalyzer("testsButcher/Square/Square.jack")
+	flag.Parse()
+
+	// Make sure we have an input path
+	if flag.NArg() != 1 {
+		_, err := fmt.Fprintf(os.Stderr, "Usage: jackcompiler <inputPath>\n")
+		if err != nil {
+			return
+		}
+		return
+	}
+
+	inputPath := flag.Arg(0)
+	//inputPath := "testsButcher/Square/Main.jack"
+
+	if inputPath == "" {
+		panic("inputPath is required")
+		return
+	}
+
+	analyzer := NewAnalyzer(inputPath)
 
 	analyzer.Analyze()
 
